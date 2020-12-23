a366ecff7c1c5a93a8bc3e93fc3c5707b9d099a8
peterlogg <peter@focaldata.com>
2020-12-23 09:45:48 +0000

    adding files for first commit

---
+++ src/Lib.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Lib
+  ( someFunc,
+  )
+where
+
+import Network.HTTP.Types
+import Web.Scotty
+
+someFunc = scotty 3000 $ do
+  get "/" $ do
+    -- handle GET request on "/" URL
+    text "This was a GET request!" -- send 'text/plain' response
+  post "/" $ do
+    text "This was a POST request!"
+  put "/" $ do
+    text "This was a PUT request!"
